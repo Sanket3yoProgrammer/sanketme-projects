0e4649881bf0222541c3e130653e97b87e37f222
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2025-03-01 04:27:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export default function YoSphereLanding() {
             Yo! Welcome
           </motion.h1>
           <motion.p 
-            className="text-lg max-w-lg mx-auto mb-6 text-gray-300 font-[Parisienne]"
+            className="text-lg max-w-lg mx-auto mb-6 text-gray-300 font-[Dancing-Script]"
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
