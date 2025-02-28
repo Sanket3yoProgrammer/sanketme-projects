e471707cb027d331f17f581c6d855e8151f27fbf
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2025-03-01 04:35:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ export default function YoSphereLanding() {
           </motion.h1>
           <motion.p 
             className="text-lg max-w-lg mx-auto mb-6 text-gray-300"
-            style={{ fontFamily: 'Jolly Lodger', fontWeight: '400' }}
+            style={{ fontFamily: 'Passions Conflict', 'cursive', fontWeight: '400' }}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
